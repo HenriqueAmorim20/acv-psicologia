e60651f775a8677946e779486f73cb2ab7126dfd
Henrique Melo <hacmelo@gmail.com>
2022-06-06 11:32:50 -0300

    feat: default bg image

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
+  <div class="main">
     <LayoutsNavbar />
     <slot />
     <LayoutsFooter />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.main {
+  background-image: url("/backgrounds/background.jpg");
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+}
+
+@media only screen and (max-width: 600px) {
+  .main {
+    background-image: url("/backgrounds/background-mobile.jpg");
+    background-position: right;
+  }
+}
+</style>
