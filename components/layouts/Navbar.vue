19c8187aea2edd2fb96e15f5d6c08d4f0200ca0d
Henrique Melo <hacmelo@gmail.com>
2022-06-08 16:09:49 -0300

    feat: navbar

--- components/layouts/Navbar.vue
+++ components/layouts/Navbar.vue
@@ -1,5 +1,287 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { onMounted, onUnmounted } from "vue";
+interface MenuItem {
+  name: string;
+  path: string;
+  id?: string;
+}
+const router = useRouter();
+let scrollPosition: number = 0;
+let drawer: Boolean = false;
+const menuItems: Array<MenuItem> = [
+  {
+    name: "sobre",
+    id: "sobreSection",
+    path: "/",
+  },
+  {
+    name: "psicoterapia",
+    id: "psicoterapiaSection",
+    path: "/",
+  },
+  {
+    name: "consultório",
+    id: "consultorioSection",
+    path: "/",
+  },
+
+  {
+    name: "vídeos",
+    id: "videosSection",
+    path: "/",
+  },
+  {
+    name: "agendar",
+    id: "agendarSection",
+    path: "/",
+  },
+  {
+    name: "publicações",
+    id: "publicacoesSection",
+    path: "/",
+  },
+];
+
+onMounted(onInit);
+onUnmounted(onDestroy);
+
+function onInit() {
+  scrollPosition = window.scrollY;
+  window.addEventListener("scroll", handleNavbar);
+}
+
+function onDestroy() {
+  window.removeEventListener("scroll", handleNavbar);
+}
+
+function handleNavbar() {
+  const navScrollEl = document.querySelector(".navbar-scroll") as HTMLElement;
+  navScrollEl.style.transform =
+    scrollPosition > window.scrollY &&
+    window.scrollY > window.innerHeight &&
+    !drawer
+      ? "translateY(0)"
+      : "translateY(-105%)";
+  scrollPosition = window.scrollY;
+}
+
+/**
+ * Handles the drawer visibility and menu icon animation
+ */
+function toggleDrawer(): void {
+  const navbarEl = document.querySelector(".navbar-normal") as HTMLElement;
+  const menuIconBars: any = document.querySelectorAll(".menu-icon-bar");
+  const drawerElement: any = document.querySelector(".drawer");
+  const logoElement: any = document.querySelector(".navbar-logo");
+
+  menuIconBars[0].classList.toggle("first-bar-active");
+  menuIconBars[1].classList.toggle("second-bar-active");
+  menuIconBars[2].classList.toggle("third-bar-active");
+  drawerElement.classList.toggle("drawer-active");
+  logoElement.classList.toggle("navbar-logo-active");
+
+  drawer = !drawer;
+  navbarEl.style.position = drawer ? "fixed" : "absolute";
+  handleNavbar();
+}
+
+/**
+ * Handles page navigation navigation and scrolling to element
+ */
+function navigateTo(item: MenuItem): void {
+  router.push({ path: item.path });
+
+  if (item.id) {
+    setTimeout(() => {
+      const element = document.getElementById(item.id);
+      const y = element?.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({ top: y, behavior: "smooth" });
+    }, 700);
+  }
+}
+</script>
 
 <template>
-  <nav class="navbar" id="navbar">navbar works!</nav>
+  <div class="navbarComponent">
+    <nav class="navbar navbar-normal">
+      <img
+        @click="navigateTo({ name: null, path: '/', id: 'homeSection' })"
+        class="navbar-logo"
+        id="navbar-logo"
+        src="/logos/logo.png"
+        alt="Ana Carolina Villaça" />
+      <div class="menu-icon" @click="toggleDrawer()">
+        <span class="menu-icon-bar"></span>
+        <span class="menu-icon-bar"></span>
+        <span class="menu-icon-bar"></span>
+      </div>
+    </nav>
+
+    <nav class="navbar navbar-scroll">
+      <img
+        @click="navigateTo({ name: null, path: '/', id: 'homeSection' })"
+        class="navbar-logo"
+        id="navbar-logo"
+        src="/logos/logo-white.png"
+        alt="Ana Carolina Villaça" />
+      <div class="menu-icon" @click="toggleDrawer()">
+        <span class="menu-icon-bar"></span>
+        <span class="menu-icon-bar"></span>
+        <span class="menu-icon-bar"></span>
+      </div>
+    </nav>
+
+    <div class="drawer">
+      <span
+        v-for="(item, index) in menuItems"
+        :key="index"
+        @click="[toggleDrawer(), navigateTo(item)]"
+        class="menu-item">
+        {{ item.name }}
+      </span>
+      <img
+        @click="
+          [
+            toggleDrawer(),
+            navigateTo({ name: null, path: '/', id: 'homeSection' }),
+          ]
+        "
+        class="drawer-logo"
+        id="drawer-logo"
+        src="/logos/logo.png"
+        alt="Ana Carolina Villaça" />
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.navbar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.4rem 1.5rem 0.4rem 0;
+  z-index: 2;
+  transition: position 0.5s ease;
+  height: 70px;
+
+  .navbar-logo {
+    width: 100px;
+    cursor: pointer;
+    visibility: visible;
+    opacity: 1;
+    transition: visibility 0.5s, opacity 0.5s ease-in-out;
+    z-index: -1;
+  }
+
+  .navbar-logo-active {
+    visibility: hidden;
+    opacity: 0;
+  }
+
+  .menu-icon {
+    cursor: pointer;
+    .menu-icon-bar {
+      height: 2px;
+      width: 40px;
+      background-color: rgba(0, 0, 0, 0.68);
+      display: block;
+      margin: 10px 0px;
+      transition: transform 0.5s, opacity 0.5s ease-in-out;
+      transform: none;
+    }
+
+    .first-bar-active {
+      transform: translateY(0.7rem) rotate(-45deg);
+    }
+    .second-bar-active {
+      transform: rotate(45deg);
+    }
+    .third-bar-active {
+      transform: rotate(45deg);
+      opacity: 0;
+    }
+  }
+}
+
+.navbar-scroll {
+  position: fixed;
+  background-color: var(--secondary);
+  transform: translateY(-80px);
+  transition: transform 0.5s ease;
+  box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.433);
+
+  .menu-icon {
+    .menu-icon-bar {
+      background-color: var(--background);
+    }
+  }
+}
+
+.drawer {
+  position: fixed;
+  inset: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  background-color: #dadada63;
+  backdrop-filter: blur(10px);
+  padding: 0 10%;
+  visibility: hidden;
+  opacity: 0;
+  z-index: 1;
+  transition: visibility 0.5s, opacity 0.5s ease-in-out;
+
+  .menu-item {
+    font-size: 3rem;
+    margin: 0.3rem 0;
+    cursor: pointer;
+  }
+  .menu-item::after {
+    display: block;
+    content: "";
+    width: 0px;
+    height: 2px;
+    background-color: var(--secondary);
+    transition: all 0.5s ease-in-out;
+  }
+  .menu-item:hover {
+    color: var(--secondary);
+  }
+  .menu-item:hover::after {
+    width: 50px;
+  }
+  .drawer-logo {
+    cursor: pointer;
+    position: absolute;
+    bottom: 3%;
+    right: 3%;
+    width: 110px;
+  }
+}
+
+.drawer-active {
+  visibility: visible;
+  opacity: 1;
+}
+
+@media only screen and (max-width: 600px) {
+  .navbar {
+    height: 60px;
+    .navbar-logo {
+      width: 90px;
+    }
+  }
+
+  .drawer {
+    .menu-item {
+      font-size: 2.2rem;
+    }
+  }
+}
+</style>
