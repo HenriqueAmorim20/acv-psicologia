7a7e007e9cb8f330257f04d01257bf827f6bbe38
Henrique Melo <hacmelo@gmail.com>
2022-06-17 17:46:00 -0300

    refactor: footer visuals

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -10,42 +10,49 @@ function scrollTop() {
 </script>
 
 <template>
-  <footer class="footer" id="footer">
+  <footer>
     <section class="left">
-      <div class="name" @click="scrollTop()">
-        <img src="/logos/logo.png" alt="Ana Carolina Villaça" />
-        <h1>Ana Carolina Villaça</h1>
-      </div>
-      <div class="social">
+      <h1 class="name">ana carolina villaça</h1>
+      <span class="description">
+        Especialista em bla bla bla bla bla. Profissional de psicoterapia com x
+        anos de experiência.
+      </span>
+      <b class="crp">CPR: 01/12930</b>
+      <div class="social-media">
         <a
           class="link"
           href="https://wa.me/5561981170400"
           target="_blank"
           rel="noopener noreferrer">
-          <Icon class="link-icon" icon="ph:whatsapp-logo-thin" />
+          <Icon class="link-icon" icon="akar-icons:whatsapp-fill" />
         </a>
         <a
           class="link"
           href="https://www.instagram.com/psicologa_anacarolinavillaca/"
           target="_blank"
           rel="noopener noreferrer">
-          <Icon class="link-icon" icon="ph:instagram-logo-thin" />
+          <Icon class="link-icon" icon="akar-icons:instagram-fill" />
         </a>
         <a
           class="link"
           href="https://www.youtube.com/channel/UCFq-F11A86ShC4ha-Crt8Yg"
           target="_blank">
-          <Icon class="link-icon" icon="arcticons:youtube" />
+          <Icon class="link-icon" icon="ant-design:youtube-outlined" />
         </a>
+        <span style="flex: 1 1 auto"></span>
       </div>
     </section>
+    <img src="/logos/logo.png" alt="" />
     <section class="right">
-      <span>
-        All rights reserved Ana Carolina Villaça &copy;
+      <span class="scrollTop" @click="scrollTop()">
+        Ir para o topo <Icon class="scroll-icon" icon="akar-icons:chevron-up" />
+      </span>
+      <span class="rights">
+        Todos os direitos reservados a Ana Carolina Villaça &copy;
         {{ new Date().getFullYear() }}
       </span>
-      <span>
-        Coded By:
+      <span class="creator">
+        Criado por:
         <a
           href="http://melohenrique.com"
           target="_blank"
@@ -58,96 +65,120 @@ function scrollTop() {
 </template>
 
 <style lang="scss" scoped>
-.footer {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: var(--background);
-  color: var(--primary);
-  padding: 1.5rem 2rem 2.5rem 0rem;
+footer {
+  display: grid;
+  grid-template-columns: 3fr 1fr 3fr;
+  padding: 3rem 1rem;
+  max-width: 1300px;
+  margin: 0 auto;
+  border-top: 1px solid lightgray;
 
   .left {
     display: flex;
     flex-direction: column;
-    align-items: center;
     .name {
+      font-family: "WorkSansThin";
+      text-transform: capitalize;
+      font-size: 2rem;
+    }
+
+    .description {
+      font-family: "WorkSansThinItalic";
+      font-size: 1rem;
+      max-width: 400px;
+      margin: 1rem 0;
+    }
+
+    .social-media {
       display: flex;
       align-items: center;
-      cursor: pointer;
-      img {
-        width: 130px;
-      }
-
-      h1 {
-        font-size: 2rem;
-        font-family: "WorkSansThin";
-      }
-    }
-
-    .social {
       margin-top: 1rem;
-      font-size: 1.8rem;
-      a {
-        margin: 0 1rem;
-        color: var(--primary);
+      .link {
+        display: flex;
+        align-items: center;
+        background-color: var(--secondary);
+        color: var(--background);
+        font-size: 1.4rem;
+        border-radius: 100%;
+        aspect-ratio: 1;
+        padding: 0.6rem;
+        margin-right: 1rem;
+        box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.319);
       }
 
-      a:hover {
-        color: var(--secondary);
+      .link:hover {
+        opacity: 0.8;
       }
     }
+  }
+
+  img {
+    width: 200px;
   }
 
   .right {
     display: flex;
     flex-direction: column;
     align-items: flex-end;
+    justify-content: center;
 
-    span {
-      margin-top: 1rem;
+    .scrollTop {
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+      font-family: "WorkSansRegular";
+      .scroll-icon {
+        margin-left: 0.5rem;
+      }
+    }
+
+    .scrollTop:hover {
+      color: var(--secondary);
+    }
+
+    .rights {
+      display: flex;
+      align-items: center;
+      margin: 1rem 0;
+      font-size: 1rem;
+      text-align: right;
+      font-family: "WorkSansThin";
+    }
+
+    .creator {
       a {
-        font-weight: bold;
         color: var(--primary);
-        text-decoration: none;
         margin-left: 0.5rem;
       }
       a:hover {
-        text-decoration: underline;
+        color: var(--secondary);
       }
     }
   }
 }
 
-@media only screen and (max-width: 800px) {
-  .footer {
-    flex-direction: column;
-    .right {
-      align-items: center;
-    }
-  }
-}
-
-@media only screen and (max-width: 450px) {
-  .footer {
-    padding: 2rem 1rem;
+@media only screen and (max-width: 780px) {
+  footer {
+    grid-template-columns: 1fr;
+    gap: 2rem;
 
     .left {
-      .name {
-        img {
-          width: 80px;
-        }
-        h1 {
-          font-size: 1.5rem;
-        }
-      }
+      align-items: center;
 
-      .social {
-        font-size: 1.5rem;
+      .description {
+        text-align: center;
       }
     }
+
     .right {
-      text-align: center;
       align-items: center;
+
+      .rights {
+        text-align: center;
+      }
+    }
+    img {
+      margin: 0 auto;
     }
   }
 }
