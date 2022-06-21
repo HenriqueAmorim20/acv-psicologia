b033f66844473aa4b55a5c0fa080f72c70e03705
Henrique Melo <hacmelo@gmail.com>
2022-06-21 13:20:59 -0300

    refactor: videos section

--- components/sections/Videos.vue
+++ components/sections/Videos.vue
@@ -1,32 +1,8 @@
 <script setup lang="ts">
-import { Icon } from "@iconify/vue";
-
-interface Video {
-  titulo: string;
-  desc: string;
-  urlFrame: string;
-  url: string;
-}
-
-const videos: Array<Video> = [
-  {
-    titulo: "Relacionamento Interpessoal/Amoroso",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet...",
-    urlFrame: "https://www.youtube.com/embed/Hkb379_CnRk",
-    url: "https://www.youtube.com/watch?v=Hkb379_CnRk",
-  },
-  {
-    titulo: "Síndrome de Burnout",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing consectetur adipisicing consectetur adipisicing consectetur adipisicing...",
-    urlFrame: "https://www.youtube.com/embed/BRGt6H6fyIQ",
-    url: "https://www.youtube.com/watch?v=BRGt6H6fyIQ",
-  },
-  {
-    titulo: "Autoestima",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    urlFrame: "https://www.youtube.com/embed/pcdHjq1UOr8",
-    url: "https://www.youtube.com/watch?v=pcdHjq1UOr8",
-  },
+const videos: Array<string> = [
+  "https://www.youtube.com/embed/Hkb379_CnRk",
+  "https://www.youtube.com/embed/BRGt6H6fyIQ",
+  "https://www.youtube.com/embed/pcdHjq1UOr8",
 ];
 </script>
 <template>
@@ -35,23 +11,15 @@ const videos: Array<Video> = [
       title="meus vídeos"
       subtitle="confira meus principais vídeos do youtube" />
     <section>
-      <div v-for="(item, index) in videos" :key="index" class="video">
+      <div class="video" v-for="(item, index) in videos" :key="index">
         <iframe
           width="100%"
           style="aspect-ratio: 560/315"
-          :src="item.urlFrame"
+          :src="item"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen></iframe>
-        <div class="content">
-          <h1>{{ item.titulo }}</h1>
-          <p>{{ item.desc }}</p>
-          <a :href="item.url" target="_blank">
-            assistir no youtube
-            <Icon class="icon" icon="akar-icons:chevron-right" />
-          </a>
-        </div>
       </div>
     </section>
     <a
@@ -73,61 +41,29 @@ const videos: Array<Video> = [
   background-color: var(--secondary);
   color: var(--background);
   min-height: fit-content;
-  padding: 5rem 0rem;
+  padding: 3rem 0rem;
 
   section {
-    max-width: 1400px;
-    margin: 5rem auto;
-    width: 100%;
     display: grid;
-    gap: 1rem;
-    padding: 0 1rem;
     grid-template-columns: repeat(3, 1fr);
-    grid-auto-rows: 1fr;
+    width: 100%;
+    max-width: 1920px;
+    margin: 4rem auto;
 
     .video {
-      display: flex;
-      flex-direction: column;
-      background-color: var(--background);
-      color: var(--secondary);
-      border-radius: 5px;
+      position: relative;
 
-      .content {
-        display: flex;
-        flex-direction: column;
-        padding: 0.5rem 1rem 1rem;
-        height: 100%;
+      &::before {
+        position: absolute;
+        inset: 0;
+        content: "";
+        display: block;
+        background-color: hsla(110, 7%, 33%, 0.281);
+      }
 
-        h1 {
-          font-size: 1.3rem;
-          font-family: "WorkSansLight";
-          margin-bottom: 0.5rem;
-        }
-
-        p {
-          margin: 1rem 0 2rem;
-          color: var(--ternary);
-        }
-
-        a {
-          display: flex;
-          align-items: center;
-          color: var(--secondary);
-          align-self: flex-end;
-          margin-top: auto;
-          text-decoration: none;
-
-          .icon {
-            margin-left: 0.3rem;
-            transition: transform 0.3s ease-in-out;
-          }
-        }
-
-        a:hover {
-          text-decoration: underline;
-          .icon {
-            transform: translateX(20%);
-          }
+      &:hover {
+        &::before {
+          display: none;
         }
       }
     }
